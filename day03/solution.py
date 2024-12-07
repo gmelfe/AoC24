af6d14f406c2f01491be941dbe7abbe42bca7b67
Gil Melfe <gmelfe@pm.me>
2024-12-07 17:52:57 +0100

    Day 03, part 2

--- day03/solution.py
+++ day03/solution.py
@@ -9,10 +9,22 @@ def read_input(file_name: str) -> str:
         return file.read()
 
 
-def parse_instructions(instructions: str) -> List[Tuple[str, str]]:
+def parse_instructions(
+    instructions: str, handle_do_and_dont: bool = False
+) -> List[Tuple[str, str]]:
     """Find all matches of the pattern mul(x,y) in the instructions string,
     where x and y are integers, and return a list of tuples (x,y).
     """
+
+    """Optionally remove all content from the instructions string that is contained between don't() and do() blocks"""
+    if handle_do_and_dont:
+        start_ignore_tag = "don't()"
+        end_ignore_tag = "do()"
+        pattern = re.compile(
+            f"{re.escape(start_ignore_tag)}.*?{re.escape(end_ignore_tag)}", re.DOTALL
+        )
+        instructions = pattern.sub("", instructions)
+
     return re.findall(r"mul\((\d+),(\d+)\)", instructions)
 
 
@@ -40,5 +52,7 @@ multiplications = parse_instructions(instructions)
 print(sum_multiplications(multiplications))
 
 # Part 2
+multiplications = parse_instructions(instructions, True)
+print(sum_multiplications(multiplications))
 
 # Done
